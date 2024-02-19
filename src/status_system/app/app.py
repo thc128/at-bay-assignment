e293423ca3cc083d96885c6f0eda5754a7b9ad28
Asaf gamliel <asafgamliel@Asafs-MacBook-Pro.local>
2024-02-19 20:10:47 +0200

    status is working

--- src/status_system/app/app.py
+++ src/status_system/app/app.py
@@ -1,15 +1,36 @@
 import logging
-from flask import Flask
-
+from flask import Flask, request
+from app.db_agent import get_document
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
+CRAWL_ID_KEY = "crawl_id"
 
 
-@app.route("/status", methods=['GET','POST'])
-def get_status():
+@app.route("/status", methods=['POST'])
+def status_handler():
     try:
-        return {"status": "All is ok"}, 200
+        crawl_id = request.form[CRAWL_ID_KEY]
+        # TODO - add CRAWL ID validation?
+    except KeyError:
+        logging.exception("Invalid input")
+        return {"status": "Invalid input",
+                 "message": "should include crawl ID in the body"}, 400
     except:
         logging.exception("An error occured")
-        return {}, 500
+        return {"status": "Error", "message": "An error occured"}, 500
+    try:
+        response = get_response(crawl_id)
+        return response, 200
+    except:
+        logging.exception("An error occured")
+        return {"status": "Error", "message": "An error occured"}, 500
+
+
+def get_response(id):
+    result = get_document(id)
+    status = "NOT_FOUND" if result is None else result.get("status")
+    response = {"status": status, "crawl_id": id}
+    if status == "COMPLETE":
+        response["file_location"] = result.get("file_location")
+    return response
